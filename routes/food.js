466fab988c38831d313eebe1c689290b91be451b
Peerawat Poombua <peerawat.pm@gmail.com>
2021-11-21 23:53:36 +0700

    added food data

--- routes/food.js
+++ routes/food.js
@@ -5,8 +5,8 @@ const Food = require('../models/food')
 const router = express.Router()
 
 router.get('/', asyncHandler(async (req, res, next) => {
-    const foods = await Food.find()
-    res.json(foods)
+    const foods = await Food.find().exec()
+    res.json(foods.map(food => ({ ...food.toJSON(), photo: `/foods/${food.photo}` })))
 }))
 
 module.exports = router
