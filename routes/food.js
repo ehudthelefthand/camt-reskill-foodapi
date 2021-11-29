08ba7ab040323280ce1c6afb0aac221a7a520772
Peerawat Poombua <peerawat.pm@gmail.com>
2021-11-29 12:04:44 +0700

    add pagination

--- routes/food.js
+++ routes/food.js
@@ -5,7 +5,13 @@ const Food = require('../models/food')
 const router = express.Router()
 
 router.get('/', asyncHandler(async (req, res, next) => {
-    const foods = await Food.find().exec()
+    let query = Food.find()
+    if (req.query.offset && req.query.limit) {
+        const offset = Number(req.query.offset)
+        const limit = Number(req.query.limit)
+        query = query.skip(offset * limit).limit(limit)
+    }
+    const foods = await query.exec()
     res.json(foods.map(food => ({ ...food.toJSON(), photo: `/foods/${food.photo}` })))
 }))
 
